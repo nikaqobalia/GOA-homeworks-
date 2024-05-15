1b800d1ab1ae9d359019bd04f18ff1057f27610b
nikaqobalia <nqobalia2007@gmail.com>
2024-05-15 04:11:20 +0400

    third commit

---
+++ Level 03/Homework/04.py
@@ -0,0 +1,7 @@
+#შევქმენი ორი ცვლადი შევინახე მასში რიცხვები და დავბეჭდე ამ ორ ცვლადს შორის არითმეტიკული მოქმედება
+age = 17
+height = 180 
+print(age + height)
+print(age - height)
+print(age * height)
+print(age / height)
